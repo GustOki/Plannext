21706657f8de3d3db328e34762b3ad5c0b67bdd5
Gustavo <gustavooki.dev@gmail.com>
2025-06-18 11:00:29 -0300

    Update routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint, jsonify, request
 
+blueprint = Blueprint('routes', __name__)
+
+tasks = []
+
+@blueprint.route('/api/tasks', methods=['GET'])
+def getTasks():
+    return jsonify(tasks)
+
+@blueprint.route('api/tasks', methods=['POST'])
+def addTasks():
+    data = request.get_json()
+    tasks.append(data)
+    return jsonify({'message': 'Tarefa adicionada!'}), 201
